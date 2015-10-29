44407fbc79630e54a2bce4900556bd9c898e3a7e
Nanosmasher <Nanosmasher@users.noreply.github.com>
2015-10-29 14:53:06 -0400

    Isotope sorting group for /canoes/
    Now can sort all the canoes with neat transitions!
    - Isotope v1.5.25 -> v2.2.2
    - portfolio, portfolio-item changed to canoe, canoe-item
    - added sorting button group and hidden isotope data
    - js.html fized quotations and lagging script put into custom.js
    - removed modernizer.js (picked up just randomly)

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,39 @@
 $(document).ready(miscelements);
 
+var $grid = $('.canoe').isotope({
+	itemSelector: '.canoe-item',
+	layoutMode: 'fitRows',
+	getSortData: {
+		year:'.iyear',
+		name:'.iname',
+		status:'.istatus',
+		stand:'.istand',
+		length:'.ilength',
+		weight: '.iweight'
+	},
+	sortAscending: {
+		year:false,
+		name:true,
+		status:true,
+		stand:true,
+		length:false,
+		weight:false
+	},
+});
+
+$(window).on('resize', resetGrid);
+	
 function miscelements(){
 	//$("#design").hover(din, dout);
-	intro();
-	pretty();
+	introFade();
+	prettyGallery();
+	resetGrid();
+	$('.iso-sort').on( 'click', 'button', function() {
+		var sortValue = $(this).attr('data-iso-sort');
+		$grid.isotope({ sortBy: sortValue });
+		$('.iso-sort button.active').removeClass("active");
+		$(this).addClass('active');
+	});
 }
 
 /*function din(){
@@ -14,16 +44,44 @@ function dout(){
     $("#design span").hide(400);
 }
 **/
+
+function introFade(){
+	setTimeout(function(){ 
+		$(".fdes").fadeIn(5000);
+	}, 1000);
+}
+
 // prettyPhoto
-function pretty(){
+function prettyGallery(){
 	jQuery('a[data-gal]').each(function() {
 		jQuery(this).attr('rel', jQuery(this).data('gal'));
 	});  	
 	jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
 }
 
-function intro(){
-	setTimeout(function(){ 
-		$(".fdes").fadeIn(1000);
-	}, 1000);
+// isotope
+function getColumnNumber() { 
+	var winWidth = $(window).width(), 
+	columnNumber = 1;
+	if (winWidth > 1200) {columnNumber = 5;}
+	else if (winWidth > 950) {columnNumber = 4;}
+	else if (winWidth > 600) {columnNumber = 3;}
+	else if (winWidth > 400) {columnNumber = 2;}
+	else if (winWidth > 250) {columnNumber = 1;}
+	return columnNumber;
+}
+
+function setColumns() {
+	var winWidth = $(window).width();
+	var columnNumber = getColumnNumber();
+	var itemWidth = Math.floor(winWidth / columnNumber);
+
+	$grid.find('.canoe-item').each(function() { 
+		$(this).css("width", itemWidth);
+	});
+}
+
+function resetGrid() { 
+	setColumns();
+	$grid.isotope('layout');
 }
